2ca25f8986497c8594a8cd107fadbf597a94feb6
swl16 <sweili165@gmail.com>
2026-09-01 01:08:19 +0800

    Merge origin/main into main
    # Conflicts:
    #	Cinema/FnBStock.txt
    #	Cinema/src/cinema/FoodPayment.java

--- Train/src/train/Train.java
+++ Train/src/train/Train.java
@@ -4,17 +4,87 @@
  */
 package train;
 
-/**
- *
- * @author WEI LI
- */
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
 public class Train {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        Scanner sc = new Scanner(System.in);
+        
+        //load graph
+        
+        while(true){
+            System.out.println("---------------------------------");
+            System.out.println("Welcome to LRT & MRT Route System");
+            System.out.println("---------------------------------");
+            System.out.println("1. Create Graph");
+            System.out.println("2. Search for a Route");
+            System.out.println("3. View the Transit Network (Console)");
+            System.out.println("4. View LRT & MRT Graph");
+            System.out.println("0. Exit");
+            System.out.println("---------------------------------");
+            System.out.println("Enter your choice: ");
+            
+            String choice = sc.nextLine();
+            
+            if(choice.equals("0")){
+                System.out.println("Exiting system. Goodbye!");
+                break;
+            }
+            
+            switch(choice){
+                case "1":
+                    graphMenu(sc);
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                default:
+                    System.out.println("Invalid input. Please try again.");
+            }
+        }
+        
+        sc.close();
     }
     
+    private static void graphMenu(Scanner sc){
+        
+        while(true){
+            System.out.println("1. Add a vertex (Station)");
+            System.out.println("2. Remove a vertex (Station)");
+            System.out.println("3. Add an edge (Connection)");
+            System.out.println("4. Remove an edge (Connection)");
+            System.out.println("0. Return");
+            System.out.println("-------------------------------");
+            System.out.println("Enter your choice: ");
+        
+            String choice = sc.nextLine();
+            
+            if(choice.equals("0")){
+                break;
+            }
+            
+            switch(choice){
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                default:
+                    System.out.println("Invalid input. Please try again.");
+            }
+        }
+    }
 }
+
+
