5ae07f8834d803499b23a41417f5108bb12f4ab1
swl16 <sweili165@gmail.com>
2026-09-02 00:54:31 +0800

    1

---
+++ Train/src/train/Displayable.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package train;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author WEI LI
+ */
+public interface Displayable {
+    
+    public int getX();
+    public int getY();
+    public String getName();
+    public Color getColor();
+    public boolean isInterchange();
+}
