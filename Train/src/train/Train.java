b9adec26b682b5c1b9a1d9a79b78a089cb09fd7e
swl16 <sweili165@gmail.com>
2026-09-01 16:28:02 +0800

    Merge origin/main into main
    # Conflicts:
    #	Cinema/FnBStock.txt
    #	Cinema/src/cinema/FoodPayment.java

--- Train/src/train/Train.java
+++ Train/src/train/Train.java
@@ -10,6 +10,8 @@ import java.util.*;
 import java.util.List;
 
 public class Train {
+    
+    private static final Map<String, List<String>> graph = new HashMap<>();
 
     public static void main(String[] args) {
        
@@ -27,7 +29,7 @@ public class Train {
             System.out.println("4. View LRT & MRT Graph");
             System.out.println("0. Exit");
             System.out.println("---------------------------------");
-            System.out.println("Enter your choice: ");
+            System.out.print("Enter your choice: ");
             
             String choice = sc.nextLine();
             
@@ -63,7 +65,7 @@ public class Train {
             System.out.println("4. Remove an edge (Connection)");
             System.out.println("0. Return");
             System.out.println("-------------------------------");
-            System.out.println("Enter your choice: ");
+            System.out.print("Enter your choice: ");
         
             String choice = sc.nextLine();
             
@@ -84,6 +86,114 @@ public class Train {
                     System.out.println("Invalid input. Please try again.");
             }
         }
+    }
+    
+    private static void addVertex(String station){
+        if(!graph.containsKey(station)){
+            graph.put(station, new ArrayList<>());
+            System.out.println("Station '" + station + "' added.");
+        }else{
+            System.out.println("Station already exists.");
+        }
+    }
+    
+    private static void removeVertex(String station){
+        if(!graph.containsKey(station)){
+            graph.values().forEach(list -> list.remove(station));
+            graph.remove(station);
+            System.out.println("Station '" + station + "' removed.");
+        }else{
+            System.out.println("Station not found.");
+        }
+    }
+    
+    private static void addEdge(String s1, String s2){
+        if(!graph.containsKey(s1)) addVertex(s1);
+        if(!graph.containsKey(s2)) addVertex(s2);
+        
+        if(!graph.get(s1).contains(s2)){
+            graph.get(s1).add(s2);
+            graph.get(s2).add(s1);
+            System.out.println("There is now a connection between " + s1 + " and " + s2 + ".");
+        }else{
+            System.out.println("Connection already exists.");
+        }
+        
+    }
+    
+    private static void removeEdge(String s1, String s2){
+        if(graph.containsKey(s1) && graph.containsKey(s2)){
+            graph.get(s1).remove(s2);
+            graph.get(s2).remove(s1);
+            System.out.println("Connection removed between " + s1 + " and " + s2 + ".");
+        }else{
+            System.out.println("One or both stations not found.");
+        }
+    }
+    
+    private static void searchRoute(Scanner sc){
+        System.out.print("Enter Starting Station: ");
+        String start = sc.nextLine().trim();
+        
+        System.out.print("Ending Station: ");
+        String end = sc.nextLine().trim();
+        
+        if(!graph.containsKey(start) || !graph.containsKey(end)){
+            System.out.println("Invalid stations. Please check the network.");
+            return;
+        }
+        
+        Queue<String> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        Map<String, String> parentMap = new HashMap<>();
+        
+        queue.add(start);
+        visited.add(start);
+        parentMap.put(start,null);
+        
+        boolean found = false;
+        
+        while(!queue.isEmpty()){
+            String currentStation = queue.poll();
+            
+            if(currentStation.equalsIgnoreCase(end)){
+                found = true;
+                break;
+            }
+            
+            for(String neighbour : graph.get(currentStation)){
+                if(!visited.contains(neighbour)){
+                    visited.add(neighbour);
+                    parentMap.put(neighbour, currentStation);
+                    queue.add(neighbour);
+                }
+            }
+        }
+        
+        if(found){
+            List<String> route = constructRoute(parentMap, end);
+            System.out.println("\nRoute found! (" + (route.size() -1) + " stops): ");
+            System.out.println(String.join("->", route));
+        }else{
+            System.out.println("\n No route found between " + start + " and " + end);
+        }
+    }
+    
+    private static List<String> constructRoute(Map<String, String> parent, String endStation){
+        List<String> route = new ArrayList<>();
+        String current = endStation;
+        
+        while(current != null){
+            route.add(0, current);
+            current = parent.get(current);
+        }
+        
+        return route;
+    }
+    
+    private static void loadGraphData(){
+        
+        addEdge("Ampang Park", "KLCC");
     }
 }
 
