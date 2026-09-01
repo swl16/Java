ae6d22109bea99735da26fad7819211eb6edc952
swl16 <sweili165@gmail.com>
2026-09-02 00:54:55 +0800

    1

--- Train/src/train/Train.java
+++ Train/src/train/Train.java
@@ -11,7 +11,9 @@ import java.util.List;
 
 public class Train {
     
-    private static final Map<String, List<String>> graph = new HashMap<>();
+    private static UnweightedGraph<Station> graph;
+    private static List<Station> stations = new ArrayList<>();
+    private static List<AbstractGraph.Edge> edges = new ArrayList<>();
 
     public static void main(String[] args) {
        
@@ -26,8 +28,7 @@ public class Train {
             System.out.println("---------------------------------");
             System.out.println("1. Create Graph");
             System.out.println("2. Search for a Route");
-            System.out.println("3. View the Transit Network (Console)");
-            System.out.println("4. View LRT & MRT Graph");
+            System.out.println("3. View LRT & MRT Graph");
             System.out.println("0. Exit");
             System.out.println("---------------------------------");
             System.out.print("Enter your choice: ");
@@ -44,10 +45,10 @@ public class Train {
                     graphMenu(sc);
                     break;
                 case "2":
+                    searchRoute(sc);
                     break;
                 case "3":
-                    break;
-                case "4":
+                    SwingUtilities.invokeLater(Train::showVisualMap);
                     break;
                 default:
                     System.out.println("Invalid input. Please try again.");
@@ -60,10 +61,8 @@ public class Train {
     private static void graphMenu(Scanner sc){
         
         while(true){
-            System.out.println("1. Add a vertex (Station)");
-            System.out.println("2. Remove a vertex (Station)");
-            System.out.println("3. Add an edge (Connection)");
-            System.out.println("4. Remove an edge (Connection)");
+            System.out.println("1. Add a Station");
+            System.out.println("2. Add a Connection");
             System.out.println("0. Return");
             System.out.println("-------------------------------");
             System.out.print("Enter your choice: ");
@@ -76,61 +75,29 @@ public class Train {
             
             switch(choice){
                 case "1":
+                    System.out.print("Enter station name: ");
+                    String name = sc.nextLine();
+                    Station newStation = new Station(name, (int)(Math.random()*600 + 50), (int)(Math.random()*300 + 50), Color.GRAY, false);
+                    graph.addVertex(newStation); 
+                    System.out.println("Added.");
                     break;
+                    
                 case "2":
+                    System.out.print("Enter Start Index: ");
+                    int u = Integer.parseInt(sc.nextLine());
+                    System.out.print("Enter End Index: ");
+                    int v = Integer.parseInt(sc.nextLine());
+                    graph.addEdge(u, v);
+                    System.out.println("Connection added.");
                     break;
-                case "3":
-                    break;
-                case "4":
-                    break;
+                    
                 default:
                     System.out.println("Invalid input. Please try again.");
             }
         }
     }
     
-    private static void addVertex(String station){
-        if(!graph.containsKey(station)){
-            graph.put(station, new ArrayList<>());
-            System.out.println("Station '" + station + "' added.");
-        }else{
-            System.out.println("Station already exists.");
-        }
-    }
     
-    private static void removeVertex(String station){
-        if(!graph.containsKey(station)){
-            graph.values().forEach(list -> list.remove(station));
-            graph.remove(station);
-            System.out.println("Station '" + station + "' removed.");
-        }else{
-            System.out.println("Station not found.");
-        }
-    }
-    
-    private static void addEdge(String s1, String s2){
-        if(!graph.containsKey(s1)) addVertex(s1);
-        if(!graph.containsKey(s2)) addVertex(s2);
-        
-        if(!graph.get(s1).contains(s2)){
-            graph.get(s1).add(s2);
-            graph.get(s2).add(s1);
-            System.out.println("There is now a connection between " + s1 + " and " + s2 + ".");
-        }else{
-            System.out.println("Connection already exists.");
-        }
-        
-    }
-    
-    private static void removeEdge(String s1, String s2){
-        if(graph.containsKey(s1) && graph.containsKey(s2)){
-            graph.get(s1).remove(s2);
-            graph.get(s2).remove(s1);
-            System.out.println("Connection removed between " + s1 + " and " + s2 + ".");
-        }else{
-            System.out.println("One or both stations not found.");
-        }
-    }
     
     private static void searchRoute(Scanner sc){
         System.out.print("Enter Starting Station: ");
@@ -139,75 +106,135 @@ public class Train {
         System.out.print("Ending Station: ");
         String end = sc.nextLine().trim();
         
-        if(!graph.containsKey(start) || !graph.containsKey(end)){
-            System.out.println("Invalid stations. Please check the network.");
-            return;
-        }
+        Station startStation = getStationByName(start);
+        Station endStation = getStationByName(end);
         
-        Queue<String> queue = new LinkedList<>();
-        Set<String> visited = new HashSet<>();
-        Map<String, String> parentMap = new HashMap<>();
         
-        queue.add(start);
-        visited.add(start);
-        parentMap.put(start,null);
-        
-        boolean found = false;
-        
-        while(!queue.isEmpty()){
-            String currentStation = queue.poll();
+        if(startStation != null && endStation != null){
+            int startIndex = graph.getIndex(startStation);
+            int endIndex = graph.getIndex(endStation);
             
-            if(currentStation.equalsIgnoreCase(end)){
-                found = true;
-                break;
-            }
+            AbstractGraph<Station>.Tree bfsTree = graph.bfs(startIndex);
+            List<Station> path = bfsTree.getPath(endIndex);
             
-            for(String neighbour : graph.get(currentStation)){
-                if(!visited.contains(neighbour)){
-                    visited.add(neighbour);
-                    parentMap.put(neighbour, currentStation);
-                    queue.add(neighbour);
-                }
+            System.out.print("Shortest Route: ");
+            for (int i = path.size() - 1; i >= 0; i--) {
+                System.out.print(path.get(i).getName() + (i > 0 ? " -> " : "\n"));
             }
-        }
-        
-        if(found){
-            List<String> route = constructRoute(parentMap, end);
-            System.out.println("\nRoute found! (" + (route.size() -1) + " stops): ");
-            System.out.println(String.join("->", route));
-        }else{
-            System.out.println("\n No route found between " + start + " and " + end);
+            SwingUtilities.invokeLater(() -> showHighlightedMap(path));
+        } else {
+            System.out.println("Station(s) not found.");
         }
     }
     
-    private static List<String> constructRoute(Map<String, String> parent, String endStation){
-        List<String> route = new ArrayList<>();
-        String current = endStation;
-        
-        while(current != null){
-            route.add(0, current);
-            current = parent.get(current);
-        }
-        
-        return route;
+    private static void showHighlightedMap(List<Station> path) {
+        JFrame frame = new JFrame("Shortest Route Found");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.add(new GraphView(graph, path));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    
+    private static void showVisualMap() {
+        JFrame frame = new JFrame("LRT & MRT Route Network");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.add(new GraphView(graph));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
     
     private static void loadGraphData(){
+        Color lrtRed = new Color(228, 56, 52);
+        Color mrtGreen = new Color(46, 161, 79);
+        Color mrtYellow = new Color(237, 193, 37);
         
-        addEdge("Ampang Park", "KLCC");
-        addEdge("KLCC", "Kampung Baru");
-        addEdge("Kampung Baru", "Dang Wangi");
-        addEdge("Dang Wangi", "Masjid Jamek");
-        addEdge("Masjid Jamek", "Pasar Seni");
-        addEdge("Ampang Park", "Persiaran KLCC");
-        addEdge("Persiaran KLCC", "Conlay");
-        addEdge("Conlay", "TRX");
-        addEdge("TRX", "Cochrane");
-        addEdge("TRX", "Chan Sow Lin");
-        addEdge("Pasar Seni", "Merdeka");
-        addEdge("Merdeka", "Bukit Bintang");
-        addEdge("Bukit Bintang", "TRX");
+        stations.add(new Station("Ampang Park", 100, 100, Color.WHITE, true)); //0
+        stations.add(new Station("KLCC", 200, 100, lrtRed, false)); // 1
+        stations.add(new Station("Kampung Baru", 300, 100,lrtRed, false)); // 2
+        stations.add(new Station("Dang Wangi", 400, 100, lrtRed, false)); // 3
+        stations.add(new Station("Masjid Jamek", 500, 100, lrtRed, false)); // 4
+        
+        stations.add(new Station("Pasar Seni", 600, 200, Color.WHITE, true)); // 5
+        stations.add(new Station("Merdeka", 500, 250, mrtGreen, false)); // 6
+        stations.add(new Station("Bukit Bintang", 400, 300, mrtGreen, false)); // 7
+        
+        stations.add(new Station("TRX", 300, 350, Color.WHITE, true)); // 8
+        
+        stations.add(new Station("Persiaran KLCC", 150, 200, mrtYellow, false)); // 9
+        stations.add(new Station("Conlay", 220, 280,  mrtYellow, false)); // 10
+        stations.add(new Station("Chan Sow Lin", 200, 420,  mrtYellow, false)); // 11
+        
+        stations.add(new Station("Cochrane", 400, 420,  mrtGreen, false)); // 12
+        
+        int[][] edgeArray ={
+            {0,1},{1,2},{2,3},{3,4},
+            {4,5},{5,6},{6,7},{7,8},
+            {0,9},{9,10},{10,8},{8,11},
+            {8,12}
+        };
+        
+        for(int[] e: edgeArray){
+            edges.add(new AbstractGraph.Edge(e[0],e[1]));
+            edges.add(new AbstractGraph.Edge(e[1],e[0]));
+        }
+        
+        graph = new UnweightedGraph<>(stations, edges);
+    }
+    
+    private static Station getStationByName(String name) {
+        for (Station s : graph.getVertices()) { 
+            if (s.getName().equalsIgnoreCase(name)) return s;
+        }
+        return null;
     }
 }
 
 
+class Station implements Displayable{
+    
+    private final String name;
+    private final int x,y;
+    
+    private final Color color;
+    private final boolean isInterchange;
+    
+    public Station(String name, int x, int y, Color color, boolean isInterchange){
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.isInterchange = isInterchange;
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+    
+    @Override
+    public Color getColor() {
+        return color;
+    }
+    
+    public boolean isInterchange() { 
+        return isInterchange; 
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
